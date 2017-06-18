0cb3e63a1716670e87e3edc409afd87d20495139
Abhilash Verma <vermaabhilash70@gmail.com>
2017-06-19 05:13:23 +0530

    ui: Add error notifier

--- routes/login.js
+++ routes/login.js
@@ -2,16 +2,25 @@ var express = require('express');
 var router = express.Router();
 var service = require('../models/users.model');
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	var params = {
-		title: 'Login to Atmos 2017',
-		methods: {
-			google: true,
-			facebook: true,
-			github: false,
-		}
-	}
+    var params = {
+        title: 'Login to Atmos 2017',
+        methods: {
+            google: true,
+            facebook: true,
+            github: true
+        }
+    };
+    if (req.query.error) {
+        if (req.query.error == "github_email_is_private") {
+            params.error = {
+                title: "Github email is private",
+                message: "Change your account settings to make your email visible or login with some other provider."
+            }
+        }
+    }
     res.render('login', params);
 });
 
