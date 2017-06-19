0837ce6987e4597d160c98106d6822fcdf935c05
Rohitt Vashishtha <aero31aero@gmail.com>
2017-06-19 21:35:26 +0530

    ui: Add temporary logo.

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var service = require('../models/users.model');
 
 var getEvent = function(n){
     var title = "Sample Event " + n;
