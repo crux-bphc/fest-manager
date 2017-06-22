6f94fb9afc9598054f1bf45fc130b06986b7d90e
Abhilash Verma <vermaabhilash70@gmail.com>
2017-06-23 00:07:04 +0530

    tools: Move all utilities to utils/

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var authenticate = require('../authentication').middleware;
+var authenticate = require('../utils/authentication').middleware;
 /* GET home page. */
 router.get('/?*', function(req, res, next) {
     res.render('index', { title: 'Home', user: req.user, route: req.url });
