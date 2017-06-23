cfa5d96695420072793a8bf20d4d4b966ed420ca
Rohitt Vashishtha <aero31aero@gmail.com>
2017-06-23 09:24:58 +0530

    routes: Eliminate /w/ hell to get direct routing.

--- routes/inner/events.js
+++ routes/inner/events.js
@@ -4,6 +4,7 @@ var eventsService = require("../services/events.model").service;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+	console.log("Me Called")
     eventsService.find(function(err, events) {
         if (err) next(err);
         res.renderState ('events', { title: 'Events', user: req.user, events: events }, function(err, string) {
