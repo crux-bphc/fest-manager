0e90fe2af0e45076661fbb3e412a25859fc091c5
Rohitt Vashishtha <aero31aero@gmail.com>
2017-06-23 08:42:04 +0530

    state: Add sidebar states.

--- utils/state.js
+++ utils/state.js
@@ -1,7 +1,9 @@
+
+
 module.exports = function(){
 	
-	var appendNavBarStates = function(state) {
-		var navbar = {}
+	var appendNavbarState = function(state) {
+		var navbar = {};
 		navbar.logout = state.isAuthenticated;
 		navbar.login = !state.isAuthenticated;
 		navbar.avatar = state.isAuthenticated;
@@ -9,14 +11,32 @@ module.exports = function(){
 		return state;
 	}
 
+	var appendSidebarState = function(state) {
+		var items = ['events', 'carts', 'account', 'settings'];
+		var sidebar = {};
+		items.forEach(function(elem) {
+			if(state.location.startsWith('/'+elem)){
+				sidebar[elem] = true;
+				if(elem == 'events'){
+					// TODO fetch event categories from db to populate
+					// sidebar.events.categories = db.events().fetch(categories).unique()
+				}
+			}
+			else {
+				sidebar[elem] = false;
+			}
+		});
+		state.sidebar = sidebar;
+		return state;
+	}
+
 	var getState = function(req) {
 		var state = {};
 		state.isAuthenticated= req.isAuthenticated();
-        state.location= req.url;
-        
-        state = appendNavBarStates(state);
-        
-        return state
+		state.location= req.url;
+		state = appendNavbarState(state);
+		state = appendSidebarState(state);
+		return state;
 	};
 	return {
 		getState,
