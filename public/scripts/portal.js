3292d59fadd8928825cfccc565759b44f515b9f6
Abhilash Verma <vermaabhilash70@gmail.com>
2017-06-24 09:12:06 +0530

    ui: Design primitive portal experience

--- public/scripts/portal.js
+++ public/scripts/portal.js
@@ -1,3 +1,9 @@
-(function (portal) {
-	portal.find('.controls .icon-add').
-})()
+(function() {
+    var portal = $("#_portal");
+
+    portal.form = function() {
+        portal.find(".add_new_item").toggleClass('collapsed');
+        $(this).toggleClass('active');
+    }
+    portal.find('.controls .icon-add').click(portal.form)
+})();
