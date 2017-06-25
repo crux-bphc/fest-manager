fb3f72437c7ee5dc4d19987d7c45f277645640ac
Rohitt Vashishtha <aero31aero@gmail.com>
2017-06-25 15:56:35 +0530

    api: Rename /services to /api and move services to api/services.

--- utils/authentication.js
+++ utils/authentication.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const config = require('../config');
-const userService = require('../routes/services/users.model').service;
+const userService = require('../routes/api/services/users').model;
 
 var findOrCreate = function (accessToken, profile, provider, done) {
 	userService.findOne({
