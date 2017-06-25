a69788ac4c251cedc0db95fc77eed342fac3afe9
Rohitt Vashishtha <aero31aero@gmail.com>
2017-06-25 17:38:08 +0530

    login: Link institute update form to /api.

--- routes/api/services/users.js
+++ routes/api/services/users.js
@@ -14,6 +14,7 @@ var usersSchema = new Schema({
 		type: String
 	},
 	teams: [Schema.Types.ObjectId],
+	events: [Schema.Types.ObjectId],
 	accommodation: Schema.Types.ObjectId,
 	token: String,
 	facebookID: String,
@@ -24,7 +25,33 @@ var usersSchema = new Schema({
 	timestamps: true
 });
 
-var model = mongoose.model('users', usersSchema);
+var model = mongoose.model('usersModels', usersSchema);
+
+router.put('/me/', function (req, res, next) {
+	console.log(req.user);
+	var body = req.body;
+	var changeddata = {};
+	console.log(body);
+	if (body.name) changeddata.name = body.name;
+	if (body.institute) changeddata.institute = body.institute;
+	console.log("Update Data:", changeddata);
+	if (req.user._id) {
+		model.update({
+			_id: req.user._id
+		}, changeddata, function (err) {
+			if (err) {
+				err.status = 500;
+				next(err);
+				return 0;
+			}
+			res.send("Success");
+		});
+	} else {
+		var err = new Error("Forbidden");
+		err.status = 403;
+		return next(err);
+	}
+});
 
 module.exports = {
 	route: '/users',
