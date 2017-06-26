12ca850bdab29ca58339c341a32c9645755ec6bb
Rohitt Vashishtha <aero31aero@gmail.com>
2017-06-26 06:17:02 +0530

    views: Add generic field view. Refactor dashboard/account.

--- routes/components/dashboard.js
+++ routes/components/dashboard.js
@@ -2,11 +2,59 @@ var express = require('express');
 var router = express.Router();
 var authenticate = require('../../utils/authentication').middleware;
 
+var getFields = function (user) {
+	var fields = [];
+	fields.push({
+		icon: "name",
+		name: "name",
+		label: "Name",
+		editable: true,
+		type: "text",
+		required: true,
+		value: user.name,
+		typeahead: false,
+		none: true,
+	});
+	fields.push({
+		icon: "email",
+		name: "email",
+		label: "Email",
+		editable: false,
+		type: "text",
+		required: true,
+		value: user.email,
+		typeahead: false,
+		none: true,
+	});
+	fields.push({
+		icon: "building",
+		name: "institute",
+		label: "Institute",
+		editable: true,
+		type: "text",
+		required: true,
+		value: user.institute,
+		typeahead: true,
+		none: true,
+	});
+	return fields;
+};
+
 /* GET users listing. */
 router.get('/', authenticate, function (req, res, next) {
 	res.renderState('dashboard', {
 		user: req.user
 	});
+});
+
+router.get('/account', authenticate, function (req, res, next) {
+	var params = {
+		title: 'My Account',
+		user: req.user,
+	};
+	params.fields = getFields(req.user);
+	console.log("Params", params);
+	res.renderState('account', params);
 });
 
 module.exports = router;
