36d2929022574ac15bc5bac76731d4377d9b267a
Rohitt Vashishtha <aero31aero@gmail.com>
2017-07-19 03:22:55 +0530

    events: Add livesearch for events.

--- public/static/scripts/events.js
+++ public/static/scripts/events.js
@@ -1,3 +1,25 @@
 $('document').ready(function () {
 	// TODO: Handle events page sorting and filtering here.
+
+	var implementSearch = function(){
+		$('input#field-search').bind('keyup change', function(e) {
+			val = $('input#field-search').val();
+			$('div.event-container').each(function (index) {
+				var elem = this;
+				$(elem).addClass('hidden');
+				var fields = [];
+				fields.push($(elem).find('span.tag').text().toLowerCase());
+				fields.push($(elem).find('h3.name').text().toLowerCase());
+				fields.push($(elem).find('span.tagline').text().toLowerCase());
+				fields.forEach(function (field, index) {
+					if (field.indexOf(val) != -1) {
+						$(elem).removeClass('hidden');
+						matched += 1;
+					}
+				});
+			});
+		});
+	};
+
+	implementSearch();
 })
