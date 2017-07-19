cb73ecf3f8f8fe0d2e21d7dde83c3bcf75064acd
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-07-19 18:59:47 +0530

    refactor: refactor event.js

--- routes/api/services/events.js
+++ routes/api/services/events.js
@@ -27,88 +27,62 @@ var model = mongoose.model('eventsModel', eventsSchema);
 
 router.post('/addtocart', function(req, res, next){
 
-		console.log("***POST init****");
-		var eventModel = model;
-		var userModel = require("./users").model;
-		var teamModel = require("./teams").model;
+	var eventModel = model;
+	var userModel = require("./users").model;
+	var teamModel = require("./teams").model;
 
-		var event_id = mongoose.Types.ObjectId(req.body.id);
+	var event_id = mongoose.Types.ObjectId(req.body.id);
 
+	eventModel.find({
+		_id: event_id
+	}, function(err, event){
+		if(err){
+			return res.json({status: 500, msg: "Error"});
+		}
+		if(typeof event[0] !== 'undefined')	{
+			var eventTeams = event[0].teams;
+			var team = new teamModel({
+				name: req.user.name,
+				members: [req.user._id],
+				event: event_id
+			});
 
-		eventModel.find({
+			team.save(function(err){
+				var team_id = team._id;
+				if(err)	{
+					return res.json({status: 500, msg: "Unable to save team in DB"});
+				}
 
-			_id: event_id
+				userModel.find({_id: req.user._id}, function(err, userDB){
+					var userEvents = userDB[0].events;
+					var userTeams = userDB[0].teams;
 
-		}, function(err, event){
+					userEvents.push(event_id);
+					userTeams.push(team_id);
 
-			if(err)
-			{
-				res.json({status: 500, msg: "Error"});
-			}
-			if(typeof event[0] !== 'undefined')
-			{
-				var eventTeams = event[0].teams;
-				var team = new teamModel({
-					name: req.user.name,
-					members: [req.user._id],
-					event: event_id
-				});
+					userModel.update({_id: req.user._id}, {events: userEvents, teams: userTeams}, function(err, num){
 
-				team.save(function(err){
-					var team_id = team._id;
-					if(err)
-					{
-						res.json({status: 500, msg: "Error"});
-					}
-					else
-					{
-
-						userModel.find({_id: req.user._id}, function(err, userDB){
-
-							var userEvents = userDB[0].events;
-							var userTeams = userDB[0].teams;
-
-							userEvents.push(event_id);
-							userTeams.push(team_id);
-
-							userModel.update({_id: req.user._id}, {events: userEvents, teams: userTeams}, function(err, num){
-
-								if(err)
-								{
-									res.json({status: 500, msg: "Error"});
-								}
-								else
-								{
-									eventTeams.push(team_id);
-
-									eventModel.update({_id: event_id}, {teams: eventTeams}, function(err, num){
-										if(err)
-										{
-											res.json({status: 500, msg: "Error"});
-										}
-										else
-										{
-											res.json({status: 200, msg: "Successful"});
-
-										}
-									});
-								}
-
-							});
-
-						});
+					if(err)	{
+						return res.json({status: 500, msg: "Unable to update user model"});
 					}
 
-				});
+					eventTeams.push(team_id);
 
-			}
-			else
-			{
-				res.json({status: 500, msg: "Error"});
-			}
-		
+					eventModel.update({_id: event_id}, {teams: eventTeams}, function(err, num){						
+						if(err)	{
+							return res.json({status: 500, msg: "Unable to update event model"});
+						}
+						res.json({status: 200, msg: "Successful"});						
+					});							
+				});
+			});				
 		});
-	});
+	}
+	else {
+		res.json({status: 500, msg: "Event Not Found"});
+	}	
+});
+});
 
 module.exports = {
 	route: '/events',
