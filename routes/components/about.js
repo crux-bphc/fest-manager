22d290d13b9d4970be4a004f4bd9d2679d5406d9
Anushree Hede <shuttler22@gmail.com>
2017-07-19 12:48:24 +0530

    frontend: Create route for Atmos About page

---
+++ routes/components/about.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+    res.renderState('about', {
+        title: 'About',
+        user: req.user
+    });
+});
+
+module.exports = router;
