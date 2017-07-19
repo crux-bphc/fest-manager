d0239419629b9eefc1cfea5866a511ae7816513f
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-07-19 18:19:37 +0530

    route: Shift the route for 'add to cart' to events.js

--- routes/api/services/events.js
+++ routes/api/services/events.js
@@ -25,6 +25,91 @@ var eventsSchema = new Schema({
 
 var model = mongoose.model('eventsModel', eventsSchema);
 
+router.post('/addtocart', function(req, res, next){
+
+		console.log("***POST init****");
+		var eventModel = model;
+		var userModel = require("./users").model;
+		var teamModel = require("./teams").model;
+
+		var event_id = mongoose.Types.ObjectId(req.body.id);
+
+
+		eventModel.find({
+
+			_id: event_id
+
+		}, function(err, event){
+
+			if(err)
+			{
+				res.json({status: 500, msg: "Error"});
+			}
+			if(typeof event[0] !== 'undefined')
+			{
+				var eventTeams = event[0].teams;
+				var team = new teamModel({
+					name: req.user.name,
+					members: [req.user._id],
+					event: event_id
+				});
+
+				team.save(function(err){
+					var team_id = team._id;
+					if(err)
+					{
+						res.json({status: 500, msg: "Error"});
+					}
+					else
+					{
+
+						userModel.find({_id: req.user._id}, function(err, userDB){
+
+							var userEvents = userDB[0].events;
+							var userTeams = userDB[0].teams;
+
+							userEvents.push(event_id);
+							userTeams.push(team_id);
+
+							userModel.update({_id: req.user._id}, {events: userEvents, teams: userTeams}, function(err, num){
+
+								if(err)
+								{
+									res.json({status: 500, msg: "Error"});
+								}
+								else
+								{
+									eventTeams.push(team_id);
+
+									eventModel.update({_id: event_id}, {teams: eventTeams}, function(err, num){
+										if(err)
+										{
+											res.json({status: 500, msg: "Error"});
+										}
+										else
+										{
+											res.json({status: 200, msg: "Successful"});
+
+										}
+									});
+								}
+
+							});
+
+						});
+					}
+
+				});
+
+			}
+			else
+			{
+				res.json({status: 500, msg: "Error"});
+			}
+		
+		});
+	});
+
 module.exports = {
 	route: '/events',
 	model: model,
--- utils/service.constructor.js
+++ utils/service.constructor.js
@@ -27,7 +27,6 @@ function service(model, router) {
 	});
 
 	router.post('/', function (req, res, next) {
-		console.log("*** POST Init ***");
 		var item = new model(req.body);
 		item.save(function (err) {
 			if (err) {
@@ -36,92 +35,6 @@ function service(model, router) {
 			}
 			res.send("Success");
 		});
-	});
-
-	router.post('/addtocart', function(req, res, next){
-
-		var eventModel = require("../routes/api/services/events").model;
-		var userModel = require("../routes/api/services/users").model;
-		var teamModel = require("../routes/api/services/teams").model;
-
-		var mongoose = require("mongoose");
-		var event_id = mongoose.Types.ObjectId(req.body.id);
-
-
-		eventModel.find({
-
-			_id: event_id
-
-		}, function(err, event){
-
-			if(err)
-			{
-				res.json({status: 500, msg: "Error"});
-			}
-			if(typeof event[0] !== 'undefined')
-			{
-				var eventTeams = event[0].teams;
-				var team = new teamModel({
-					name: req.user.name,
-					members: [req.user._id],
-					event: event_id
-				});
-
-				team.save(function(err){
-					var team_id = team._id;
-					if(err)
-					{
-						res.json({status: 500, msg: "Error"});
-					}
-					else
-					{
-
-						userModel.find({_id: req.user._id}, function(err, userDB){
-
-							var userEvents = userDB[0].events;
-							var userTeams = userDB[0].teams;
-
-							userEvents.push(event_id);
-							userTeams.push(team_id);
-
-							userModel.update({_id: req.user._id}, {events: userEvents, teams: userTeams}, function(err, num){
-
-								if(err)
-								{
-									res.json({status: 500, msg: "Error"});
-								}
-								else
-								{
-									eventTeams.push(team_id);
-
-									eventModel.update({_id: event_id}, {teams: eventTeams}, function(err, num){
-										if(err)
-										{
-											res.json({status: 500, msg: "Error"});
-										}
-										else
-										{
-											res.json({status: 200, msg: "Successful"});
-
-										}
-									});
-								}
-
-							});
-
-						});
-					}
-
-				});
-
-			}
-			else
-			{
-				res.json({status: 500, msg: "Error"});
-			}
-		
-		});
-		console.log("\n\n");
 	});
 
 	router.put('/', function (req, res, next) {
