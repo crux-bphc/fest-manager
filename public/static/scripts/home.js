bf5ea5618934324ee7ce61f861e1a046f682b1fa
Abhilash Verma <vermaabhilash70@gmail.com>
2017-07-20 23:15:17 +0530

    countdown: Fix Date.parse() related bug

--- public/static/scripts/home.js
+++ public/static/scripts/home.js
@@ -1,7 +1,7 @@
 (function() {
 	var count = document.getElementById('count');
 	var time = function() {
-		count.innerHTML = Math.ceil((Date.parse('10-27-2017') - Date.now())/(24*3600*1000));
+		count.innerHTML = Math.ceil((1509042600000 - Date.now())/(24*3600*1000));
 	}
 	time();
 	window.setInterval(time,60000);
