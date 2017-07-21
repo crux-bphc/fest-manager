d33090fbb2888d0205a7b15f849612d2c065136c
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-07-21 20:01:08 +0530

    events: Make 'add to cart' and 'join team' buttons functional

--- public/static/scripts/events.js
+++ public/static/scripts/events.js
@@ -39,3 +39,113 @@ $('document').ready(function () {
 
 	implementSearch();
 })
+
+$(".add_to_cart").click(function(){
+
+	$.ajax({
+		method: "POST",
+		url: "/api/events/addtocart",
+		beforeSend: function(xhr){
+			xhr.setRequestHeader("Client", "Fest-Manager/dash");
+		},
+		data: {
+			id: this.id
+		},
+		success: function(res){
+			if(res.status == 200)
+			{
+				if(typeof res.teamID !== 'undefined')
+				{
+					swal({
+						title: "Successful",
+						text: "Event added to cart ! Your team ID is " + res.teamID,
+						type: "success",
+						confirmButtonText: "OK",
+						confirmButtonColor: "#202729"
+					});
+				}
+				else
+				{
+				swal({
+						title: "Successful",
+						text: "Event added to cart !",
+						type: "success",
+						confirmButtonText: "OK",
+						confirmButtonColor: "#202729"
+					});}
+			}
+			else
+			{
+				swal({
+					title: "Failed !",
+					text: res.msg,
+					type: "error",
+					confirmButtonText: "OK",
+					confirmButtonColor: "#202729"
+				});
+			}
+		}
+	});
+});
+
+$(".join_team").click(function(){
+
+	swal({
+	  title: "Join Team",
+	  text: "Enter the team id to proceed",
+	  type: "input",
+	  showCancelButton: true,
+	  closeOnConfirm: false,
+	  animation: "slide-from-top",
+	  inputPlaceholder: "Team ID",
+	  confirmButtonColor: "#202729",
+	  confirmButtonText: "Join",
+	  showLoaderOnConfirm: true
+	},
+	function(inputValue){
+	  if (inputValue === false) return false;
+	  
+	  if (inputValue === "") {
+	    swal.showInputError("You need to write something!");
+	    return false
+	  }
+
+	  if(inputValue.length != 24) {
+	    swal.showInputError("Enter a valid 24 character team ID");
+	    return false
+	  }
+	  
+	  $.ajax({
+		method: "POST",
+		url: "/api/events/jointeam",
+		beforeSend: function(xhr){
+			xhr.setRequestHeader("Client", "Fest-Manager/dash");
+		},
+		data: {
+			id: inputValue
+		},
+		success: function(res){
+			if(res.status == 200)
+			{
+				swal({
+					title: "Successful",
+					text: "You joined the team",
+					type: "success",
+					confirmButtonText: "OK",
+					confirmButtonColor: "#202729"
+				});
+			}
+			else
+			{
+				swal({
+					title: "Failed !",
+					text: res.msg,
+					type: "error",
+					confirmButtonText: "OK",
+					confirmButtonColor: "#202729"
+				});
+			}
+		}
+	});
+});
+});
