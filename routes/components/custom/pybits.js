4f36b8791eb1335dc33386baecdaf2f11ef3f66a
Vaibhav Bhasin <bhasinvaibhav97@gmail.com>
2017-07-23 04:26:59 +0000

    pybits: improve styles

--- routes/components/custom/pybits.js
+++ routes/components/custom/pybits.js
@@ -27,46 +27,46 @@ var applyStateChanges = function (req) {
 };
 
 router.get('/', function (req, res, next) {
-	req.stateparams.pagetitle = 'PyBits';
+	req.stateparams.pagetitle = 'PyBITS';
 	req = applyStateChanges(req);
 	res.renderState('custom/pybits/home', {
-		title: 'PyBits',
+		title: 'PyBITS',
 		user: req.user,
 	});
 });
 
 router.get('/talks', function (req, res, next) {
-	req.stateparams.pagetitle = 'PyBits Talks';
+	req.stateparams.pagetitle = 'PyBITS Talks';
 	req = applyStateChanges(req);
 	res.renderState('custom/pybits/talks', {
-		title: 'PyBits Talks',
+		title: 'PyBITS Talks',
 		user: req.user,
 	});
 });
 
 router.get('/sprints', function (req, res, next) {
-	req.stateparams.pagetitle = 'PyBits Sprints';
+	req.stateparams.pagetitle = 'PyBITS Sprints';
 	req = applyStateChanges(req);
 	res.renderState('custom/pybits/sprints', {
-		title: 'PyBits Sprints',
+		title: 'PyBITS Sprints',
 		user: req.user,
 	});
 });
 
 router.get('/workshops', function (req, res, next) {
-	req.stateparams.pagetitle = 'PyBits Workshops';
+	req.stateparams.pagetitle = 'PyBITS Workshops';
 	req = applyStateChanges(req);
 	res.renderState('custom/pybits/workshops', {
-		title: 'PyBits Workshops',
+		title: 'PyBITS Workshops',
 		user: req.user,
 	});
 });
 
 router.get('/2016', function (req, res, next) {
-	req.stateparams.pagetitle = 'PyBits 2016';
+	req.stateparams.pagetitle = 'PyBITS 2016';
 	req = applyStateChanges(req);
 	res.renderState('custom/pybits/2016', {
-		title: 'PyBits 2016',
+		title: 'PyBITS 2016',
 		user: req.user,
 	});
 });
