8eb7a8178fbe67eaadb50afa31ea8e872a1586f2
root <root@bphc.dota>
2017-08-12 22:54:39 +0000

    tools: Add ca-export script.

---
+++ utils/export.js
@@ -0,0 +1,2 @@
+print("name|institute|email|address|phone|pincode|why");
+db.usersmodels.find({isAmbassador: true}, {"name":true,"email":true,"address":true,"why": true, "pincode":true,"phone":true,"institute":true}).forEach(function(user){print(user.name+"|"+user.institute+"|"+user.email+"|"+user.address.replace(/[\n\r]+/g, "").replace(/ +/g, " ")+"|"+user.phone+"|"+user.pincode+"|"+user.why.replace(/[\n\r]+/g, "").replace(/ +/g, " "));});
