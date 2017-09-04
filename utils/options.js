8f168f06699ca7652f83caad665af0b92da6bf53
Abhilash Verma <vermaabhilash70@gmail.com>
2017-09-04 18:39:43 +0530

    options: Call appendUserProfilePic and fix profile pic bug.

--- utils/options.js
+++ utils/options.js
@@ -1,26 +1,27 @@
-module.exports = function() {
+module.exports = function () {
 
-    var appendUserProfilePic = function(options) {
-        if (options.user) {
-            if (options.user.googleID) {
-                if (options.user.profileImage.endsWith("sz=50")) {
-                    options.user.profileImage = options.user.profileImage.replace('sz=50', 'sz=500');
-                }
-            } else if (options.user.facebookID) {
-                options.user.profileImage = "https://graph.facebook.com/v2.9/" + options.user.facebookID + "/picture?type=large";
-            } else if (options.user.githubID) {
-                options.user.profileImage = "https://avatars1.githubusercontent.com/u/" + options.user.githubID;
-            } else {
-                options.user.profileImage = "https://www.miastograf.pl/bundles/miastografmain/images/avatar.png";
-            }
-        }
-        return options;
-    } ;
+	var appendUserProfilePic = function (options) {
+		if (options.user) {
+			if (options.user.googleID) {
+				if (options.user.profileImage.endsWith("sz=50")) {
+					options.user.profileImage = options.user.profileImage.replace('sz=50', 'sz=500');
+				}
+			} else if (options.user.facebookID) {
+				options.user.profileImage = "https://graph.facebook.com/v2.9/" + options.user.facebookID + "/picture?type=large";
+			} else if (options.user.githubID) {
+				options.user.profileImage = "https://avatars1.githubusercontent.com/u/" + options.user.githubID;
+			} else {
+				options.user.profileImage = "https://www.miastograf.pl/bundles/miastografmain/images/avatar.png";
+			}
+		}
+		return options;
+	};
 
-    var updateOptions = function(options) {
-        return options;
-    };
-    return {
-        updateOptions,
-    };
+	var updateOptions = function (options) {
+		options = appendUserProfilePic(options);
+		return options;
+	};
+	return {
+		updateOptions,
+	};
 }();
