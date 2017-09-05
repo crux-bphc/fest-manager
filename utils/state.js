dc262d50be45b7936d914bca55f432c0ab488fc5
Abhilash Verma <vermaabhilash70@gmail.com>
2017-09-06 03:14:05 +0530

    grunt: More backlog.

--- utils/state.js
+++ utils/state.js
@@ -25,7 +25,7 @@ module.exports = function () {
 				user.avatar = "https://graph.facebook.com/v2.9/" + user.facebookID + "/picture?type=large";
 			}
 			if (req.user.institute && req.user.phone) {
-				user.profile = true; 	//True if profile is complete
+				user.profile = true; //True if profile is complete
 			}
 		}
 		state.user = user;
