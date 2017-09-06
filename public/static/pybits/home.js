8b5e50ae4f53bc51599dbf7f4ba574db1599977d
Rohitt Vashishtha <aero31aero@gmail.com>
2017-09-06 20:26:13 +0530

    pybits: Add typed subtitle.

---
+++ public/static/pybits/home.js
@@ -0,0 +1,13 @@
+$('document').ready(function() {
+    var options = {
+        strings: [
+            "Conference^1000",
+            "Workshops^1000",
+            "Code Sprints^1000",
+        ],
+        typeSpeed: 50,
+        smartBackspace: true,
+        loop: true,
+    }
+    var typed = new Typed('#typedholder', options);
+})
