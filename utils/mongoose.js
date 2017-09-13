f9365aa7e597e35af8ac925dd2ec62b6f129cf23
Rohitt Vashishtha <aero31aero@gmail.com>
2017-09-13 10:26:13 +0530

    tests: Run tests on a separate disposable database.

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -3,12 +3,13 @@ var connection = function (callback) {
 	var mongoose = require('mongoose');
 	mongoose.Promise = global.Promise;
 	const config = require('./config-loader');
-	var mongoDB = config.database.url + config.database.name;
+	var databasename = process.env.FM_DATABASE || config.database.name || "fest-manager-default";
+	var mongoDB = config.database.url + databasename;
 	mongoose.connect(mongoDB);
 	var db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 	db.on('connected', function () {
-		console.log("Connected");
+		console.log("Connected to", databasename);
 		if (callback) callback(null);
 	});
 };
