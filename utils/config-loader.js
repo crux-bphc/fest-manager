526e7a881928fd0d44835ff76d56600fe376a482
Rohitt Vashishtha <aero31aero@gmail.com>
2017-09-19 04:05:26 +0530

    config: Improve error handling. Improve log message.

--- utils/config-loader.js
+++ utils/config-loader.js
@@ -1,6 +1,20 @@
-// Simple utility to get the path to config file.
+// Simple utility to load the config file.
 var projectroot = require('project-root-path');
 var configfile = process.env.NODE_ENV == 'production' ? 'config.js' : 'dev-config.js';
 configfile = projectroot + "/" + configfile;
 console.log("config-loader: Trying to use:", configfile);
-module.exports = require(configfile);
+var config;
+try {
+	config = require(configfile);
+} catch (e) {
+	console.error("config-loader: Failed to load config. Trying to use dev config.");
+	configfile = projectroot + "/" + 'dev-config.js';
+	try {
+		config = require(configfile);
+	} catch (e) {
+		console.log("config-loader: Failed to load dev-config. Something is seriously wrong.");
+		console.log("config-loader: You have seriously messed up the repository perhaps. Consider recloning.");
+		process.exit(2);
+	}
+}
+module.exports = config;
