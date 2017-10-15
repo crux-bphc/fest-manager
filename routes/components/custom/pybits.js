f6b5d4c1932457ad753208c8b1634a1fb7b22efd
Vaibhav Bhasin <bhasinvaibhav97@gmail.com>
2017-10-15 21:38:57 +0530

    pybits: Add talks

--- routes/components/custom/pybits.js
+++ routes/components/custom/pybits.js
@@ -59,6 +59,50 @@ router.get('/talks', function (req, res, next) {
 	});
 });
 
+router.get('/talks/saikat', function (req, res, next) {
+	req.stateparams.pagetitle = 'Talk by Saikat Kumar Dey';
+	req = applyStateChanges(req);
+	req.stateparams.subtitle = "PyBITS Talks";
+	res.renderState('custom/pybits/talks/saikat', {
+		title: 'Building a chatbot from scratch using Open-Source tools',
+		user: req.user,
+		pybits: true,
+	});
+});
+
+router.get('/talks/ramanathan', function (req, res, next) {
+	req.stateparams.pagetitle = 'Talk by Ramanathan Muthaiah';
+	req = applyStateChanges(req);
+	req.stateparams.subtitle = "PyBITS Talks";
+	res.renderState('custom/pybits/talks/ramanathan', {
+		title: 'Introduction to Continuous Integration in SW Development',
+		user: req.user,
+		pybits: true,
+	});
+});
+
+router.get('/talks/satish', function (req, res, next) {
+	req.stateparams.pagetitle = 'Talk by Satish Rao';
+	req = applyStateChanges(req);
+	req.stateparams.subtitle = "PyBITS Talks";
+	res.renderState('custom/pybits/talks/satish', {
+		title: 'Internet of Things (IoT) and Python',
+		user: req.user,
+		pybits: true,
+	});
+});
+
+router.get('/talks/madhu', function (req, res, next) {
+	req.stateparams.pagetitle = 'Talk by Madhu Vadlamani';
+	req = applyStateChanges(req);
+	req.stateparams.subtitle = "PyBITS Talks";
+	res.renderState('custom/pybits/talks/madhu', {
+		title: 'Impact of Python in Data Science',
+		user: req.user,
+		pybits: true,
+	});
+});
+
 router.get('/sprints', function (req, res, next) {
 	req.stateparams.pagetitle = 'PyBITS Sprints';
 	req = applyStateChanges(req);
