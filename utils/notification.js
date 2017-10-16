0ddbb3eb3e0985975d9160751ce6bb5420ecfbd2
Abhilash Verma <vermaabhilash70@gmail.com>
2017-10-16 22:03:33 +0530

    notifications: Add moment.js. Push notifications with current timestamp.

--- utils/notification.js
+++ utils/notification.js
@@ -1,6 +1,6 @@
 const projectRoot = require('project-root-path');
 const userService = require(projectRoot + '/routes/api/services/users').model;
-
+const moment = require('moment');
 /**
 Usage Details :
 
@@ -11,16 +11,16 @@ var notification = require("PATH-TO-notification.js");
 notification.createNotification(req.user._id, {
 	title: "Test Notification",
 	message: "Some one line text here ......",
-	icon: "Random.ico",
-	image: "Path-to-image",
+	icon: "icon-<choose from icomoon/font.css>",
 	time: "Some Timestamp",
-	read: true or false
-
+	read: false,
+	route: "where-the-notification-leads-to"
 });
 
 **/
 
 function pushToMany(id, notification){
+	notification.date = moment().format('DD/MM/YY hh:mm A');
 	userService.update({_id: id},{$push: {notifications: notification}} ,function(){
 		console.log("Notification Added");
 	});
