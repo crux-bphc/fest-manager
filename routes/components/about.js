9623e142bccb7d1c8f5161d59ba2e67c50f3344e
Rohitt Vashishtha <aero31aero@gmail.com>
2017-10-21 14:33:33 +0530

    about: Restructure pages to match site. Revert to older css.

--- routes/components/about.js
+++ routes/components/about.js
@@ -12,8 +12,20 @@ var applyStateChanges = function (req) {
 			route: "/about/contact",
 		},
 		{
+			label: "Accommodation",
+			route: "/about/accommodation",
+		},
+		{
 			label: "Reaching here",
 			route: "/about/map",
+		},
+		{
+			label: "Terms of Service",
+			route: "/about/service",
+		},
+		{
+			label: "Privacy Policy",
+			route: "/about/privacy",
 		},
 		{
 			label: "ATMOS 2016",
@@ -52,9 +64,40 @@ router.get('/map', function (req, res, next) {
 
 router.get('/history', function (req, res, next) {
 	req.stateparams.pagetitle = 'History';
+	req.stateparams.subtitle = 'History of Atmos';
 	req = applyStateChanges(req);
 	res.renderState('about/history', {
 		title: 'History of Atmos',
+		user: req.user
+	});
+});
+
+router.get('/accommodation', function (req, res, next) {
+	req.stateparams.pagetitle = 'Accommodation';
+	req.stateparams.subtitle = 'Accommodation';
+	req = applyStateChanges(req);
+	res.renderState('about/accommodation', {
+		title: 'Accommodation Options',
+		user: req.user
+	});
+});
+
+router.get('/privacy', function (req, res, next) {
+	req.stateparams.pagetitle = 'Privacy Policy';
+	req.stateparams.subtitle = 'Privacy Policy';
+	req = applyStateChanges(req);
+	res.renderState('about/privacy', {
+		title: 'Privacy Policy',
+		user: req.user
+	});
+});
+
+router.get('/service', function (req, res, next) {
+	req.stateparams.pagetitle = 'Terms Of Service';
+	req.stateparams.subtitle = 'Terms Of Service';
+	req = applyStateChanges(req);
+	res.renderState('about/service', {
+		title: 'Terms Of Service',
 		user: req.user
 	});
 });
