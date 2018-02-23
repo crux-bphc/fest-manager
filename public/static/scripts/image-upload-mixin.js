55670acec9796eeb80262531445064fcf45e8ef3
Rohitt Vashishtha <aero31aero@gmail.com>
2018-02-23 15:07:17 +0530

    home: Reddit front-end lazy load images.

--- public/static/scripts/image-upload-mixin.js
+++ public/static/scripts/image-upload-mixin.js
@@ -15,11 +15,13 @@ function imgCropInit(data) {
 					data: result
 				}
 			}).done(function (response) {
+				console.log("Successfully uploaded image");
 				$(editor).find('.status i').attr('class', '').addClass('icon-check');
 				$(editor).find('.status').attr('title', 'Upload succeeded');
 				$(editor).find('.status').addClass("success");
 				$(editor).attr('value', response.path);
 			}).fail(function (jqxhr, textStatus, err) {
+				console.log("Failed to upload image");
 				$(editor).find('.status').attr('title', 'Upload failed');
 				$(editor).find('.status i').attr('class', '').addClass('icon-close');
 				$(editor).find('.status').addClass("success");
