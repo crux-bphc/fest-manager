832b1be793ad9b554231b2352b431ebb4e72cf1b
Nishant <nishant23799@gmail.com>
2018-07-29 03:12:23 +0530

    Atmos18 landing page: add the coutdown

--- public/static/dist/js/app.js
+++ public/static/dist/js/app.js
@@ -22,7 +22,32 @@
         precision: "mediump"
     });
 
+     //setup the countdown
+     function timer() {
 
+        console.log("hello");
+        var endTime = new Date("26 October 2018 00:00:00 GMT+05:30");           
+        endTime = (Date.parse(endTime) / 1000);
+
+        var now = new Date();
+        now = (Date.parse(now) / 1000);
+
+        var timeLeft = endTime - now;
+
+        var days = Math.floor(timeLeft / 86400); 
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+        if (hours < "10") { hours = "0" + hours; }
+        if (minutes < "10") { minutes = "0" + minutes; }
+        if (seconds < "10") { seconds = "0" + seconds; }
+
+        $("#days").html(days+"<small><small><small>days</small></small></small>");
+        $("#hours").html(hours+"<small><small><small>hours</small></small></small>");
+        $("#minutes").html(minutes+"<small><small><small>mins</small></small></small>");
+        $("#seconds").html(seconds+"<small><small><small>secs</small></small></small>");      
+}
 
 setInterval(function() { timer(); }, 1000);
 
