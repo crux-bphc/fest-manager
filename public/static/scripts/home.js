b29e93b63b7bd63950e8e0734be35f2407de3687
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-08-19 17:01:43 +0530

    landing: updated countdown timer

--- public/static/scripts/home.js
+++ public/static/scripts/home.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Oct 26, 2018 12:00:00").getTime();
+var countDownDate = new Date("Oct 26, 2018 00:00:00").getTime();
 
 var x = setInterval(function() {
     var now = new Date().getTime();
