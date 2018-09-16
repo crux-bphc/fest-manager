6acd5f34441e7b41cd2a653e91a3fd1e12b3db8c
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-09-16 15:41:25 +0530

    home: remove disableScroll function as it was not needed.

--- public/static/scripts/home.js
+++ public/static/scripts/home.js
@@ -35,13 +35,4 @@ function preventDefaultForScrollKeys(e) {
     }
 }
 
-function disableScroll() {
-    if (window.addEventListener) // older FF
-    window.addEventListener('DOMMouseScroll', preventDefault, false);
-    window.onwheel = preventDefault; // modern standard
-    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-    window.ontouchmove  = preventDefault; // mobile
-    document.onkeydown  = preventDefaultForScrollKeys;
-}
-
-disableScroll();
+document.onkeydown  = preventDefaultForScrollKeys;
