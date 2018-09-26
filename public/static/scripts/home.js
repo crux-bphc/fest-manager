4b0a427488101de0b8228c50f4177c992e89f510
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-09-27 00:55:33 +0530

    home: Complete landing page.

--- public/static/scripts/home.js
+++ public/static/scripts/home.js
@@ -16,18 +16,23 @@ let london = (function (){
             currentPage--;
         $('.scroll-wrapper').css('transform', 'translateY('+  -currentPage * window.innerHeight +'px)');
     };
-    
     $(window).keydown(function(e) {
         console.log('window-event');
+        
         if(e.which === 40) {
             console.log('event'+currentPage);
             if(currentPage == 0)
-            {
+            {   
                 $('#page-2 .places .top').addClass('start');
                 $('#page-2 .places .bottom').addClass('start');
                 $('#page-2 .places .yr').addClass('start');
                 $('#page-2 .places .nm').addClass('start');
                 $('#page-2 .places .tickets').addClass('start');
+            }
+            if(currentPage == 1) {
+                $('#page-3 #background').addClass('start');
+                $('#page-3 #background #building').addClass('start');
+                $('#page-3 #background #content-flex').addClass('start');
             }
             showNextPage();
         }
@@ -36,8 +41,32 @@ let london = (function (){
             showPreviousPage();
         }  
     });
-    if(currentPage === 1) {
+    // catching-scroll
+    $('#page-2').bind('inview', function (event, visible, topOrBottomOrBoth) {
+        console.log('catch-scroll');
+        if (visible == true) {
+            $('#page-2 .places .top').addClass('start');
+            $('#page-2 .places .bottom').addClass('start');
+            $('#page-2 .places .yr').addClass('start');
+            $('#page-2 .places .nm').addClass('start');
+            $('#page-2 .places .tickets').addClass('start');
+          // element is now visible in the viewport
+          if (topOrBottomOrBoth == 'top') {
+            $('#page-2 .places .top').addClass('start');
+            $('#page-2 .places .bottom').addClass('start');
+            $('#page-2 .places .yr').addClass('start');
+            $('#page-2 .places .nm').addClass('start');
+            $('#page-2 .places .tickets').addClass('start');
+            // top part of element is visible
+          } else if (topOrBottomOrBoth == 'bottom') {
+            // bottom part of element is visible
+          } else {
+            // whole part of element is visible
+          }
+        } else {
+          // element has gone out of viewport
+        }
+      });
 
-    }
 
 })();
