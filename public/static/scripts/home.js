9b4bc9acc1766c6aa1142377127b3c1e7332ff59
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-09-26 18:01:28 +0530

    Home: Add new landing page layout.

--- public/static/scripts/home.js
+++ public/static/scripts/home.js
@@ -1,38 +1,42 @@
-var countDownDate = new Date("Oct 26, 2018 00:00:00").getTime();
+let london = (function (){
+    console.log('in home js');
+    var currentPage = 1;
+    let scrollDiv = $('.hash-home');
+    let getTargetHeight = (page)=>{   
+        return page*window.innerHeight;
+    };
 
-var x = setInterval(function() {
-    var now = new Date().getTime();
+    let maxLength = 5;
+    
+    let showNextPage = ()=>{
+        console.log('insShowNextPage',getTargetHeight(currentPage));
+        if(currentPage < maxLength)
+            currentPage++;
+        $('.scroll-wrapper').css('transform', 'translateY('+ -currentPage * window.innerHeight +'px)');
+    };
 
-    var duration = countDownDate - now;
-
-    var days = Math.floor(duration / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((duration % (1000 * 60)) / 1000);
-
-    document.getElementById("countdown").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds + "<br> DAYS TO GO";
-
-    if (duration < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-}, 1000);
-
-
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
-
-function preventDefault(e) {
-    e = e || window.event;
-    if (e.preventDefault)
-    e.preventDefault();
-    e.returnValue = false;
-}
-
-function preventDefaultForScrollKeys(e) {
-    if (keys[e.keyCode]) {
-        preventDefault(e);
-        return false;
-    }
-}
-
-document.onkeydown  = preventDefaultForScrollKeys;
+    let showPreviousPage = ()=>{
+        console.log('inShowPreviousPage',getTargetHeight(currentPage));
+        if(currentPage > 1)
+            currentPage--;
+        $('.scroll-wrapper').css('transform', 'translateY('+  -currentPage * window.innerHeight +'px)');
+    };
+    
+    $(window).keydown(function(e) {
+        console.log('window-event');
+        if(e.which === 40) {
+            console.log('event');
+            showNextPage();
+            
+        }
+        if(e.which === 38) {
+            console.log('event-up-key');
+            showPreviousPage();
+        }  
+    });
+    
+    return {
+        showNextPage : showNextPage,
+        showPreviousPage: showPreviousPage
+    };
+})();
