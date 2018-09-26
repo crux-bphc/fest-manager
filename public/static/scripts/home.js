884534be3c22fce15e09cb0286ad4d5872cea242
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-09-27 05:05:50 +0530

    home: Fixed scroll bug.

--- public/static/scripts/home.js
+++ public/static/scripts/home.js
@@ -5,23 +5,7 @@ let london = (function (){
     
     let showNextPage = ()=>{
         //console.log('insShowNextPage',getTargetHeight(currentPage));
-        if(currentPage < maxLength)
-            currentPage++;
-        $('.scroll-wrapper').css('transform', 'translateY('+ -currentPage * window.innerHeight +'px)');
-    };
-
-    let showPreviousPage = ()=>{
-        //console.log('inShowPreviousPage',getTargetHeight(currentPage));
-        if(currentPage > 0)
-            currentPage--;
-        $('.scroll-wrapper').css('transform', 'translateY('+  -currentPage * window.innerHeight +'px)');
-    };
-    $(window).keydown(function(e) {
-        console.log('window-event');
-        
-        if(e.which === 40) {
-            console.log('event'+currentPage);
-            if(currentPage == 0)
+        if(currentPage == 0)
             {   
                 $('#page-2 .places .top').addClass('start');
                 $('#page-2 .places .bottom').addClass('start');
@@ -34,6 +18,23 @@ let london = (function (){
                 $('#page-3 #background #building').addClass('start');
                 $('#page-3 #background #content-flex').addClass('start');
             }
+        if(currentPage < maxLength)
+            currentPage++;
+        $('.scroll-wrapper').css('transform', 'translateY('+ -currentPage * window.innerHeight +'px)');
+    };
+
+    let showPreviousPage = ()=>{
+        //console.log('inShowPreviousPage',getTargetHeight(currentPage));
+        if(currentPage > 0)
+            currentPage--;
+        $('.scroll-wrapper').css('transform', 'translateY('+  -currentPage * window.innerHeight +'px)');
+    };
+
+    $(window).keydown(function(e) {
+        console.log('window-event');
+        
+        if(e.which === 40) {
+            console.log('event'+currentPage);
             showNextPage();
         }
         if(e.which === 38) {
@@ -41,32 +42,27 @@ let london = (function (){
             showPreviousPage();
         }  
     });
-    // catching-scroll
-    $('#page-2').bind('inview', function (event, visible, topOrBottomOrBoth) {
-        console.log('catch-scroll');
-        if (visible == true) {
-            $('#page-2 .places .top').addClass('start');
-            $('#page-2 .places .bottom').addClass('start');
-            $('#page-2 .places .yr').addClass('start');
-            $('#page-2 .places .nm').addClass('start');
-            $('#page-2 .places .tickets').addClass('start');
-          // element is now visible in the viewport
-          if (topOrBottomOrBoth == 'top') {
-            $('#page-2 .places .top').addClass('start');
-            $('#page-2 .places .bottom').addClass('start');
-            $('#page-2 .places .yr').addClass('start');
-            $('#page-2 .places .nm').addClass('start');
-            $('#page-2 .places .tickets').addClass('start');
-            // top part of element is visible
-          } else if (topOrBottomOrBoth == 'bottom') {
-            // bottom part of element is visible
-          } else {
-            // whole part of element is visible
-          }
-        } else {
-          // element has gone out of viewport
-        }
+    function mouseWheelWrapper() {
+    $(window).bind("mousewheel", function(e) {
+        console.log(e.originalEvent.deltaY);
+         (function scroll() {
+            if(e.originalEvent.wheelDelta < 0) {
+                //scroll down
+                showNextPage();
+                console.log('Down');
+            } else {
+                //scroll up
+                console.log('Up');
+                showPreviousPage();
+            }
+         })();
+         
+         $(window).unbind("mousewheel");//unbinding to restrict one scroll to showing one page.
+         setTimeout(mouseWheelWrapper, 700);
+         return false;
+         
       });
-
+    }
+    mouseWheelWrapper();
 
 })();
