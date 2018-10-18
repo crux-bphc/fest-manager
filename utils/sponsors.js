76bf0c2e9ad89f8379c22f0df2da424a07b59ea5
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-10-18 16:13:33 +0530

    views: Add sponsors.

--- utils/sponsors.js
+++ utils/sponsors.js
@@ -1,9 +1,77 @@
 let sponsors = [
     {
+        link:"https://google.com",
+        img:"/static/images/sponsors/Jio.png",
+        imgalt:"",
+        meta:"DIGITAL LIFE PARTNER"
+    },
+    {   
+        link:"",
+        img:"/static/images/sponsors/timken-big.jpg",
+        imgalt:"",
+        meta:"CO-TITLE"
+
+    },
+    {   
+        link:"",
+        img:"/static/images/sponsors/ibm.png",
+        imgalt:"",
+        meta:"POWERED BY IBM"
+
+    },
+    {
+        link:"",
+        img:"/static/images/sponsors/opgc.jpg",
+        imgalt:"",
+        meta:"CO-POWERED BY OPGC"
+    },
+    {
         link:"",
         img:"/static/images/sponsors/Abhibus_Logo.jpg",
         imgalt:"",
+        meta:"Bus Partner"
+    },
+    {   
+        link:"",
+        img:"/static/images/sponsors/download.png",
+        imgalt:"",
+        meta:"TITLE SPONSOR-ENIGMA"
+
+    },
+    {   
+        link:"",
+        img:"/static/images/sponsors/worldquant.png",
+        imgalt:"",
+        meta:"Alphathon Partner"
+
+    },
+    {   
+        link:"",
+        img:"/static/images/sponsors/manhattan.jpg",
+        imgalt:"",
         meta:""
+
+    },
+    {   
+        link:"",
+        img:"/static/images/sponsors/princeton_review.png",
+        imgalt:"",
+        meta:""
+
+    },
+    {   
+        link:"",
+        img:"/static/images/sponsors/MBA_Talent_Hunt.jpg",
+        imgalt:"",
+        meta:""
+
+    },
+    {   
+        link:"",
+        img:"/static/images/sponsors/els-international.png",
+        imgalt:"",
+        meta:""
+
     },
     {
         link:"",
@@ -13,25 +81,39 @@ let sponsors = [
     },
     {   
         link:"",
+        img:"/static/images/sponsors/z-drive.jpg",
+        imgalt:"",
+        meta:""
+
+    },
+    {   
+        link:"",
+        img:"/static/images/sponsors/make-marketer.jpg",
+        imgalt:"",
+        meta:""
+
+    },
+    {   
+        link:"",
+        img:"/static/images/sponsors/doritos.png",
+        imgalt:"",
+        meta:""
+
+    },
+    {   
+        link:"",
+        img:"/static/images/sponsors/zebronics.jpeg",
+        imgalt:"",
+        meta:""
+
+    },
+    {   
+        link:"",
         img:"/static/images/sponsors/aiesec.png",
         imgalt:"",
         meta:""
 
-    },
-    {   
-        link:"",
-        img:"/static/images/sponsors/dkb.png",
-        imgalt:"",
-        meta:""
-
-    },
-    {   
-        link:"",
-        img:"/static/images/sponsors/download.png",
-        imgalt:"",
-        meta:""
-
-    },
+    },    
     {   
         link:"",
         img:"/static/images/sponsors/excelr-solutions-madhapur-hyderabad-digital-marketing-training-institutes-naghyq.jpg",
@@ -42,27 +124,6 @@ let sponsors = [
     {   
         link:"",
         img:"/static/images/sponsors/fever104.png",
-        imgalt:"",
-        meta:""
-
-    },
-    {   
-        link:"",
-        img:"/static/images/sponsors/glanbia.jpg",
-        imgalt:"",
-        meta:""
-
-    },
-    {   
-        link:"",
-        img:"/static/images/sponsors/ibm.png",
-        imgalt:"",
-        meta:""
-
-    },
-    {   
-        link:"",
-        img:"/static/images/sponsors/lakme.jpg",
         imgalt:"",
         meta:""
 
@@ -90,20 +151,6 @@ let sponsors = [
     },
     {   
         link:"",
-        img:"/static/images/sponsors/manhattan.jpg",
-        imgalt:"",
-        meta:""
-
-    },
-    {   
-        link:"",
-        img:"/static/images/sponsors/MBA_Talent_Hunt.jpg",
-        imgalt:"",
-        meta:""
-
-    },
-    {   
-        link:"",
         img:"/static/images/sponsors/mm.png",
         imgalt:"",
         meta:""
@@ -118,27 +165,6 @@ let sponsors = [
     },
     {   
         link:"",
-        img:"/static/images/sponsors/parx.jpg",
-        imgalt:"",
-        meta:""
-
-    },
-    {   
-        link:"",
-        img:"/static/images/sponsors/princeton_review.png",
-        imgalt:"",
-        meta:""
-
-    },
-    {   
-        link:"",
-        img:"/static/images/sponsors/sbi.jpg",
-        imgalt:"",
-        meta:""
-
-    },
-    {   
-        link:"",
         img:"/static/images/sponsors/slice-pay-logo.jpg",
         imgalt:"",
         meta:""
@@ -146,21 +172,14 @@ let sponsors = [
     },
     {   
         link:"",
-        img:"/static/images/sponsors/timken.png",
+        img:"/static/images/sponsors/wittyfeed1.jpg",
         imgalt:"",
         meta:""
 
     },
     {   
         link:"",
-        img:"/static/images/sponsors/worldquant.png",
-        imgalt:"",
-        meta:""
-
-    },
-    {   
-        link:"",
-        img:"/static/images/sponsors/zebronics.jpeg",
+        img:"/static/images/sponsors/dkb.png",
         imgalt:"",
         meta:""
 
