faf5d89f0353d4506664949d7b7bca98526e9099
Ashwik <ashwikreddyaileni999@gmail.com>
2018-12-13 23:18:11 +0530

    front end: changes to home page,contact us  & fonts

--- dev-config.js
+++ dev-config.js
@@ -62,16 +62,16 @@ var config = {
     },
 
     strings: {
-        name: "ATMOS",
-        tagline: "Technical Fest of Bits Pilani Hyderabad Campus",
+        name: "ARENA",
+        tagline: "SPORTS Fest of Bits Pilani Hyderabad Campus",
         organiser: "BITS Pilani Hyderabad Campus",
         theme: "Into the Sixth Dimension",
-        duration: "26th - 28th October",
-        description: "ATMOS is the annual techno-management fest of Birla Institute of Technology and Science, Pilani – Hyderabad Campus. Right from its inception in 2012, it ...",
+        duration: "24th - 27th JANUARY",
+        description: "ARENA is the annual sports fest of Birla Institute of Technology and Science, Pilani – Hyderabad Campus. Right from its inception in 2012, it ...",
         keywords: "Atmos, bphc, bits, technical",
-        url: "https://atmos.bits-hyd.org/",
+        url: "https://atmos.bits-arena.com/",
         image: "https://atmos.bits-hyd.org/static/images/meta.png",
-        twitter: "@BITSAtmos",
+        twitter: "@BITS_Arena",
         registrationLink: "https://www.google.co.in/",
     }
 };
